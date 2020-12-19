6923e97b9a4d4d36b938d84d3191ed1004eede14
saguiguilid <tbki.jea@gmail.com>
2020-12-19 22:31:45 +0800

    18 - Background Slider

---
+++ 18-background-slider/script.js
@@ -0,0 +1,34 @@
+const body = document.body
+const slides = document.querySelectorAll(".slide")
+const leftBtn = document.getElementById("left")
+const rightBtn = document.getElementById("right")
+
+let activeSlide = 0
+
+setBackgroundImage()
+
+function setBackgroundImage() {
+  console.log(activeSlide, slides.length)
+  body.style.backgroundImage = slides[activeSlide].style.backgroundImage
+}
+
+function setActiveSlide() {
+  slides.forEach(slide => slide.classList.remove("active"))
+  slides[activeSlide].classList.add("active")
+}
+
+leftBtn.addEventListener("click", () => {
+  activeSlide--
+  if (activeSlide < 0) activeSlide = slides.length - 1
+
+  setBackgroundImage()
+  setActiveSlide
+})
+
+rightBtn.addEventListener("click", () => {
+  activeSlide++
+  if (activeSlide > slides.length - 1) activeSlide = 0
+
+  setBackgroundImage()
+  setActiveSlide()
+})
