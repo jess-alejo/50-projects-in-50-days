ccbc756a2f051ec45efd2771dc091db171e80845
saguiguilid <tbki.jea@gmail.com>
2021-01-09 12:56:14 +0800

    Relayout index page

---
+++ projects.js
@@ -0,0 +1,176 @@
+const projects = [
+  {
+    url: "./01-expanding-cards/index.html",
+    title: "Expanding Cards",
+    description: "TODO: add some description to the purpose of this project",
+  },
+  {
+    url: "./02-progress-steps/index.html",
+    title: "Progress Steps",
+    description: "TODO: add some description to the purpose of this project",
+  },
+  {
+    url: "./03-rotating-navigation/index.html",
+    title: "Rotating Navigation",
+  },
+  {
+    url: "./04-hidden-search-widget/index.html",
+    title: "Hidden Search Widget",
+  },
+  {
+    url: "./05-blurry-loading/index.html",
+    title: "Blurry Loading",
+    description: "TODO: add some description to the purpose of this project",
+  },
+  {
+    url: "./06-scroll-animation/index.html",
+    title: "Scroll Animation",
+    description: "TODO: add some description to the purpose of this project",
+  },
+  {
+    url: "./07-split-landing-page/index.html",
+    title: "Split Landing Page",
+    description: "TODO: add some description to the purpose of this project",
+  },
+  {
+    url: "./08-form-wave-animation/index.html",
+    title: "Form Wave Animation",
+  },
+  {
+    url: "./09-sound-board/index.html",
+    title: "Sound Board",
+    description: "TODO: add some description to the purpose of this project",
+  },
+  {
+    url: "./10-dad-jokes/index.html",
+    title: "Dad Jokes",
+    description: "TODO: add some description to the purpose of this project",
+  },
+  {
+    url: "./11-event-keycodes/index.html",
+    title: "Event KeyCodes",
+    description: "TODO: add some description to the purpose of this project",
+  },
+  {
+    url: "./12-faq-boxes/index.html",
+    title: "FAQ Boxes",
+    description: "TODO: add some description to the purpose of this project",
+  },
+  {
+    url: "./13-random-choice-picker/index.html",
+    title: "Random Choice Picker",
+  },
+  {
+    url: "./14-animated-navigation/index.html",
+    title: "Animated Navigation",
+  },
+  {
+    url: "./15-incrementing-counter/index.html",
+    title: "Incrementing Counter",
+  },
+  {
+    url: "./16-drink-water/index.html",
+    title: "Drink Water",
+    description: "TODO: add some description to the purpose of this project",
+  },
+  {
+    url: "./17-movie-app/index.html",
+    title: "Movie App",
+    description: "TODO: add some description to the purpose of this project",
+  },
+  {
+    url: "./18-background-slider/index.html",
+    title: "Background Slider",
+    description: "TODO: add some description to the purpose of this project",
+  },
+  {
+    url: "./19-theme-clock/index.html",
+    title: "Theme Clock",
+    description: "TODO: add some description to the purpose of this project",
+  },
+  {
+    url: "./20-button-ripple-effect/index.html",
+    title: "Button Ripple Effect",
+  },
+  {
+    url: "./21-drag-n-drop/index.html",
+    title: "Drag N Drop",
+    description: "TODO: add some description to the purpose of this project",
+  },
+  {
+    url: "./22-drawing-app/index.html",
+    title: "Drawing App",
+    description: "TODO: add some description to the purpose of this project",
+  },
+  {
+    url: "./23-kinetic-css-loader/index.html",
+    title: "Kinetic CSS Loader",
+    description: "TODO: add some description to the purpose of this project",
+  },
+  {
+    url: "./24-content-placeholder/index.html",
+    title: "Content Placeholder",
+  },
+  {
+    url: "./25-sticky-navbar/index.html",
+    title: "Sticky Navbar",
+    description: "TODO: add some description to the purpose of this project",
+  },
+  {
+    url: "./26-double-vertical-slider/index.html",
+    title: "Double Vertical Slider",
+  },
+  {
+    url: "./27-toast-notification/index.html",
+    title: "Toast Notification",
+    description: "TODO: add some description to the purpose of this project",
+  },
+  {
+    url: "./28-github-profiles/index.html",
+    title: "Github Profiles",
+    description: "TODO: add some description to the purpose of this project",
+  },
+  {
+    url: "./29-double-heart-click/index.html",
+    title: "Double Hearth Click",
+  },
+  {
+    url: "./30-auto-text-effect/index.html",
+    title: "Auto Text Effect",
+    description: "TODO: add some description to the purpose of this project",
+  },
+  {
+    url: "./31-password-generator/index.html",
+    title: "Password Generator",
+    description: "TODO: add some description to the purpose of this project",
+  },
+  {
+    url: "./32-good-cheap-fast-checkboxes/index.html",
+    title: "Good, Cheap, Fast",
+  },
+  {
+    url: "./33-notes-app/index.html",
+    title: "Notes App",
+    description: "TODO: add some description to the purpose of this project",
+  },
+  {
+    url: "./34-animated-countdown/index.html",
+    title: "Animated Countdown",
+    description: "TODO: add some description to the purpose of this project",
+  },
+  {
+    url: "./35-image-carousel/index.html",
+    title: "Image Carousel",
+    description: "TODO: add some description to the purpose of this project",
+  },
+  {
+    url: "./36-hoverboard/index.html",
+    title: "Hoverboard",
+    description: "TODO: add some description to the purpose of this project",
+  },
+  {
+    url: "./37-pokedex/index.html",
+    title: "Pokedex",
+    description: "TODO: add some description to the purpose of this project",
+  },
+]
